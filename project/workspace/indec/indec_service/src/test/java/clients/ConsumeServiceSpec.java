6e14ddf9511d8b5943b0e820883e373811155116
Marcelo Manuel Baraschi <manuel.baraschi @gmail.com>
2019-03-14 16:43:14 -0300

    update ConsumeServiceSpec

--- project/workspace/indec/indec_service/src/test/java/clients/ConsumeServiceSpec.java
+++ project/workspace/indec/indec_service/src/test/java/clients/ConsumeServiceSpec.java
@@ -13,19 +13,19 @@ import static org.junit.Assert.fail;
 public class ConsumeServiceSpec {
 
     @Test
-    public void test_axis_one_health(){
-
+    public void test_axis_one_health(){ //Walmart
         final String endpointUrl = "http://localhost:8000/supermercado_axis_one/services/SupermercadoAxisOne.SupermercadoAxisOneHttpEndpoint/";
+        //final String wsdlUrl = "http://localhost:8000/supermercado_axis_one/services/SupermercadoAxisOne?wsdl";
         final String targetNameSpace = "http://ws.SupermercadoAxisOne/";
         final HashMap<String, String> params = new HashMap<>();
         params.put("endpointUrl", endpointUrl);
         params.put("targetNameSpace", targetNameSpace);
-
+        //params.put("wsdlUrl", wsdlUrl);
         final  Optional<SupermercadosServiceContract> maybeClient =
-                ClientFactory.getInstance().getClientFor(ClientType.AXIS, params);
+                ClientFactory.getInstance().getClientFor(ClientType.CXF, params);
         assertTrue(maybeClient.isPresent());
         try {
-            final String confirmation = maybeClient.get().health("GOB");
+            final String confirmation = maybeClient.get().health("INDEC");
             System.out.println(confirmation);
             assertTrue(true);
         } catch (ClientException e) {
@@ -34,7 +34,7 @@ public class ConsumeServiceSpec {
     }
 
     @Test
-    public void test_cxf_one_health(){
+    public void test_cxf_one_health(){ //Jumbo
         final String wsdlUrl = "http://localhost:8003/supermercado_cxf_one/services/supermercado_cxf_one?wsdl";
         final HashMap<String, String> params = new HashMap<>();
         params.put("wsdlUrl", wsdlUrl);
@@ -42,7 +42,7 @@ public class ConsumeServiceSpec {
                 ClientFactory.getInstance().getClientFor(ClientType.CXF, params);
         assertTrue(maybeClient.isPresent());
         try {
-            final String confirmation = maybeClient.get().health("GOB");
+            final String confirmation = maybeClient.get().health("INDEC");
             System.out.println(confirmation);
             assertTrue(true);
         } catch (ClientException e) {
@@ -52,23 +52,20 @@ public class ConsumeServiceSpec {
     }
 
     @Test
-    public void test_rest_one_health(){
+    public void test_rest_one_health(){ //Carrefour
         final String url = "http://localhost:8001/supermercado_rest_one/supermercadoRestOne";
         final HashMap<String, String> params = new HashMap<>();
         params.put("url", url);
         final Optional<SupermercadosServiceContract> maybeClient =
                 ClientFactory.getInstance().getClientFor(ClientType.REST, params);
-
         assertTrue(maybeClient.isPresent());
         try {
-            final String confirmation = maybeClient.get().health("GOB");
+            final String confirmation = maybeClient.get().health("INDEC");
             System.out.println(confirmation);
             assertTrue(true);
         } catch (ClientException e) {
             fail();
         }
     }
-
-
 
 }
